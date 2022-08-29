914b1c70a22bc494288fb50a8b6554567daaa430
Natalia MC <nvmoncas@gmail.com>
2022-08-29 11:41:51 -0500

    menu scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,19 @@ document.getElementById("navbar-open").addEventListener("click", function(){
   var navbar = document.getElementById("navbar-id");
   var navbarContain = document.getElementById("nav-contain");
   var navbarClose = document.getElementById("nav-close");
+  var body = document.getElementById("body");
   navbar.classList.toggle("navbar-mobile-open");
   navbarContain.classList.toggle("navbar-container-mobile");
   navbarClose.classList.toggle("navbar-close-button");
+  body.classList.toggle("body-mobile-open");
 });
 document.getElementById("navbar-close").addEventListener("click", function(){
   var navbar = document.getElementById("navbar-id");
   var navbarContain = document.getElementById("nav-contain");
+  var body = document.getElementById("body");
   var navbarClose = document.getElementById("nav-close");
   navbar.classList.remove("navbar-mobile-open");
   navbarContain.classList.remove("navbar-container-mobile");
   navbarClose.classList.remove("navbar-close-button");
+  body.classList.remove("body-mobile-open");
 });
